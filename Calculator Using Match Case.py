19e2157dcb179c38c6c3c2bb17b31a17dc8954f1
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-20 01:25:45 +0530

    Updating Content

--- Calculator Using Match Case.py
+++ Calculator Using Match Case.py
@@ -1,8 +1,8 @@
 a= int(input("Enter first number:- "))
 b= int(input("Enter Second number:- "))
 c= input("Enter the Operation to be performed:- ")
-match c:
 
+match c:
     case "Addition":
         print(a+b)
     case "Subtraction":
