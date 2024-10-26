de30b935a7cb17bb33678f050d2c23fbd2492277
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-26 21:50:31 +0530

    Updating Content

--- Calculator Using Match Case.py
+++ Calculator Using Match Case.py
@@ -1,15 +1,22 @@
 a= int(input("Enter first number:- "))
+
 b= int(input("Enter Second number:- "))
+
 c= input("Enter the Operation to be performed:- ")
 
 match c:
+
     case "Addition":
         print(a+b)
+
     case "Subtraction":
         print(a-b)
+
     case "Multiplication":
         print(a*b)
+
     case "Division":
         print(a/b)
+
     case _:
         print("No Valid Operation Provided.")
