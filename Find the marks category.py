cd6db59670b5be2ce07fa21804e214a201e85cc2
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-27 13:01:30 +0530

    Updating Content

---
+++ Find the marks category.py
@@ -0,0 +1,13 @@
+a= int(input("Enter Marks:- "))
+if(a>=90):
+    print("A")
+elif(a>=80):
+    print("B")
+elif(a>=70):
+    print("C")
+elif(a>=60):
+    print("D")
+elif(a>=50):
+    print("E")
+else:
+    print("F")
