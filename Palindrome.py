445ac86d269f6af431cd3203b98aa7549f1de48b
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-14 00:04:15 +0530

    Adding Content

---
+++ Palindrome.py
@@ -0,0 +1,11 @@
+# Palindrome
+a=input("Enter the String:- ")
+b=0
+for i in range (len(a)//2):
+    if(a[i]!=a[-i-1]):
+        b=1
+        break
+if(b==0):
+    print(a, "is Palindrome.")
+else:
+    print(a, "is not Palindrome.")
