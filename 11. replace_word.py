ec465662163b9c9a908df4b9f4cf9437c492b7ef
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-01-15 13:56:29 +0530

    Adding replace_word
    This file helps us replace some characters with other character from a string.

---
+++ 11. replace_word.py
@@ -0,0 +1,29 @@
+"""
+This module helps us replace certain words in a string.
+"""
+
+def rplce (a :str )->str :
+    """
+    Replace a part of string and returns the modified string given a.
+
+    Args:
+        a(string)= string
+    
+    Return:
+        string= replaced string.
+    """
+    return(a.replace("banana", "samose"))
+    
+# Using key- value pair to handle possible KeyboardInterrupt error by user.
+try:
+    # Entering the string
+    string:str= "maine 200 banana khaye"
+
+    # Modifying the string using rplace function
+    mod_string:str= rplce(string)
+
+    # Printing the modified string
+    print(mod_string)   
+
+except KeyboardInterrupt as e:
+    print(f"Error: User terminated the program beforehand.")
