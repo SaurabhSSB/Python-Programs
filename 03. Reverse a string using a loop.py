61ef59871ca9e51d4abdf1a4ba34595817cae030
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-03-09 20:38:40 +0530

    Adding reverse a string program file

---
+++ 03. Reverse a string using a loop.py
@@ -0,0 +1,24 @@
+a="Termination"
+c=""
+for i in range(len(a),0,-1):
+    c=c+a[i-1]
+
+print(c)
+
+
+
+
+text = "Python"
+reversed_text = ""
+for char in text:
+    reversed_text = char + reversed_text
+print("Reversed:", reversed_text)
+
+
+
+
+a= "retard"
+b= ""
+for i in a:
+    b= i+b
+print(b)
